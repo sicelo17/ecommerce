6268a481487d8d1774abe63a04df43b939007a3c
sicelo17 <70627871+sicelo17@users.noreply.github.com>
2020-12-24 13:15:56 +0200

    responsive navbar

--- js/main.js
+++ js/main.js
@@ -3,6 +3,25 @@
 Navigation
 ===================
 */
+const navOpen = document.querySelector(".nav-hamburger");
+const navClose = document.querySelector(".close-toggle");
+const menu = document.querySelector(".nav-menu");
+const navContainer = document.querySelector(".nav-menu");
+
+navOpen.addEventListener("click", () => {
+    menu.classList.add("open");
+    document.body.classList.add("active");
+    navContainer.style.left = "0";
+    navContainer.style.width = "30rem";
+})
+
+navClose.addEventListener("click", () => {
+    menu.classList.remove("open");
+    document.body.classList.remove("active");
+    navContainer.style.left = "-30rem";
+    navContainer.style.width = "0";
+})
+
 
 /*
 ===================
